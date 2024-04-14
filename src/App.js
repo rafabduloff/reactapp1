1e89cb6da43beb22762e33d3b12c3555cff0b240
rafabduloff <rafabduloff@mail.ru>
2024-04-14 23:36:56 +0500

    Added button

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+class App extends React.Component{
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>fds</button>
+      </div>
+    );
+  }
+} 
+
+
+export default App;
