eab55847046db453bb1a077f16588684d2cfdeca
rafabduloff <rafabduloff@mail.ru>
2024-04-15 11:18:12 +0500

    added form.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Main from "./components/Main";
+import form from "./components/form";
 
 class App extends React.Component{
   render() {
---
+++ src/components/form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class form extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Погода</h2>
+                <p>Узнайте погоду в вашем регионе</p>
+            </div>
+        );
+    }
+}
+
+export default form;
